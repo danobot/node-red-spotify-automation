990715c38caef3498e699187d5c880d173e6c324
Daniel Mason <danielbaker210@gmail.com>
2022-11-29 08:29:05 +0800

    append works

--- node-red-contrib-cache/src/append-context/append-context.js
+++ node-red-contrib-cache/src/append-context/append-context.js
@@ -2,12 +2,17 @@ module.exports = function (RED) {
 
     function AppendContextNode(config) {
         RED.nodes.createNode(this, config);
-
         this.on('input', function (msg) {
+            let globalContext = this.context().global;
+            console.log("context ", globalContext.keys())
+            let storedValue = globalContext.get(config.key) || []
+            console.log("payload ", msg.payload.length)
+            console.log("storedValue ", storedValue.length)
             
-            let value = context.global.get(config.key) || []
-            value = value.concat(msg.payload)
-            global.set(config.key, value)
+            let concatValue = storedValue.concat(msg.payload)
+            console.log("concatValue ", concatValue.length)
+            globalContext.set(config.key, concatValue)
+
             this.send(msg);
         });
        
--- node-red-contrib-cache/src/use-context/use-context.js
+++ node-red-contrib-cache/src/use-context/use-context.js
@@ -4,19 +4,8 @@ module.exports = function (RED) {
     function AppendContextNode(config) {
         RED.nodes.createNode(this, config);
         let globalContext = this.context().global;
-        console.log("Use Context Global context ", globalContext.keys())
         this.on('input', function (msg) {
-            console.log("Use Context Global context uibside", globalContext.keys())
-            
-             
-            let value = globalContext.get(config.key)||[]
-            // if (globalContext.keys().includes(config.name)) {
-                
-            //     console.log("Use Context: loaded: ", value.length)
-            // } else {
-            //     console.log("Use Context: Context key does not exist.", config.key)
-            // }
-            msg.payload = value;
+            msg.payload = globalContext.get(config.key)||[];
             this.send(msg);
         });
        
